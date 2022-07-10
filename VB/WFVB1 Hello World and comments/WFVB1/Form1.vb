8841f0ba5e74184e800db1d9943c18ebb60e7f60
Majeh <zmajeh@gmail.com>
2022-07-10 21:05:31 +0530

    Hello World and comments

---
+++ VB/WFVB1 Hello World and comments/WFVB1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' This is comment
+        MessageBox.Show("Hello world")
+    End Sub
+End Class
