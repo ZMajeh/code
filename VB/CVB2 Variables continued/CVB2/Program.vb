bd557a6921ae714b0284ca8e803cfbb01be67a6a
Majeh <zmajeh@gmail.com>
2022-07-12 21:40:16 +0530

    Variables continued

---
+++ VB/CVB2 Variables continued/CVB2/Program.vb
@@ -0,0 +1,28 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        ' Variables can also be assigned like this
+        ' Following is list of type of basic sata types
+        Dim v1 As Single = 0.89
+        Dim v2 As Boolean = True
+        Dim v3 As Byte = 255
+        Dim v6 As Decimal = 1024
+        Dim v7 As Double = 512
+        Dim v8 As Int32 = 2048
+        Dim v9 As Int64 = 16384
+        Dim v11 As Short = 1000
+        Dim v12 As String = "ZMajeh"
+
+        Console.WriteLine("V1: " + v1.ToString())
+        Console.WriteLine("V2: " + v2.ToString())
+        Console.WriteLine("V3: " + v3.ToString())
+        Console.WriteLine("V6: " + v6.ToString())
+        Console.WriteLine("V7: " + v7.ToString())
+        Console.WriteLine("V8: " + v8.ToString())
+        Console.WriteLine("V9: " + v9.ToString())
+        Console.WriteLine("V11: " + v11.ToString())
+        Console.WriteLine("V12: " + v12.ToString())
+
+    End Sub
+End Module
