eee16246cf4bdb7f05c7662912e99bc541b213a3
Majeh <zmajeh@gmail.com>
2022-07-13 21:03:28 +0530

    Variables

---
+++ CPP/2_Variables/p2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main()
+{
+    int v1=10;
+    float v2=1.954f;
+    double v3=1.999;
+    bool v4=true;
+    std::string v5="ZMAJEH";
+    char v6='Z';
+
+    std::cout << "\nV1 : " << v1;
+    std::cout << "\nV2 : " << v2;
+    std::cout << "\nV3 : " << v3;
+    std::cout << "\nV4 : " << v4;
+    std::cout << "\nV5 : " << v5;
+    std::cout << "\nV6 : " << v6;
+
+
+    return 0;
+}
