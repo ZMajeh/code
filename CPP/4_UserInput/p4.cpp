c7052d9403ac66e7513006d153aedaa28b8936bd
Majeh <zmajeh@gmail.com>
2022-07-13 22:50:11 +0530

    User Input

---
+++ CPP/4_UserInput/p4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cin >> x;
+    std::cout << "You entered : " << x << std::endl;
+
+    std::string y;
+    std::cin >> y;
+    std::cout << "You entered : " << y << std::endl;
+    return 0;
+}
