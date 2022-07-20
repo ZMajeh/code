2e72a64f15571e5e553ec99d7839febbefc5e596
Majeh <zmajeh@gmail.com>
2022-07-20 20:41:10 +0530

    Do until loop

---
+++ VB/CVB6 Do until loop/CVB6/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a As Integer = 0
+        Do Until a >= 10
+            a += 1
+            Console.WriteLine(a)
+        Loop
+    End Sub
+End Module
