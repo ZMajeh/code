7097647266e86543ed49993a4c0616048bd247b5
Majeh <zmajeh@gmail.com>
2022-07-20 20:40:11 +0530

    While loop

---
+++ VB/CVB4 While loop/CVB4/Program.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim a As Integer = 0
+        While a < 10
+            a += 1
+            Console.WriteLine(a)
+        End While
+    End Sub
+End Module
