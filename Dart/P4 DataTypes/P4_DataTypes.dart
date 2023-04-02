112e8b9749012f254d5224aced482c18b623500e
ZMajeh <zmajeh@gmail.com>
2023-04-02 13:28:25 +0000

    Data types

---
+++ Dart/P4 DataTypes/P4_DataTypes.dart
@@ -0,0 +1,16 @@
+int x = 100;
+double y = 10.01;
+String name = "Majeh";
+Runes nameAgain = new Runes("Triya");
+bool itsTrue = true;
+bool itsFalse = false;
+
+List myList = ["Majeh", "Triya"];
+Map myMap = { "Name":"Majeh"};
+
+void main()
+{
+  print("Integer $x\nDouble $y\nString $name\nRunes $nameAgain\nBool True $itsTrue\nBool False $itsFalse");
+  print("Map data ${myMap["Name"]}");
+  print("List data ${myList[0]}, ${myList[1]}");
+}
