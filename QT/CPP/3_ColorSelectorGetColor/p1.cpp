bb7fc8c9bc64471a1021c3f3c37d5f0f4926c8bd
ZMajeh <zmajeh@gmail.com>
2023-12-20 04:19:17 +0000

    Get color with getColor of QColorDialog

---
+++ QT/CPP/3_ColorSelectorGetColor/p1.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <QColorDialog>
+#include <QPushButton>
+#include <QColor>
+#include <QDialog>
+#include <QPalette>
+#include <QObject>
+
+// Free function to open color dialog and change button color
+void openColors(QPushButton *mpb)
+{
+    
+
+    mpb->setAutoFillBackground(true);
+    QColor myColor = QColorDialog::getColor();
+    QPalette palette = mpb->palette();
+    // Text color
+    palette.setColor(QPalette::Button, myColor);
+    // Button border
+    palette.setColor(QPalette::Window, myColor);
+    mpb->setPalette(palette);
+    mpb->repaint();
+}
+
+int main(int c, char **a)
+{
+    QApplication myApp(c, a);
+    QPushButton *mb = new QPushButton("Change Color");//, (QWidget*)&myApp);
+    // connect signal to lambda function
+    QObject::connect(mb, &QPushButton::clicked, [mb](){openColors(mb);});
+    mb->show();
+    // Wait for dialog to close
+    return myApp.exec();
+}
+
